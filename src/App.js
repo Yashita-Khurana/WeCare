961f76e71d1f950e48e2c80a1272103847f6c6ff
Yashita <yashita1109.cse19@chitkara.edu.in>
2021-05-12 19:06:34 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Navbar />
     </div>
   );
 }
