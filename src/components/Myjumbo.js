da708c4d7c3bea64f5ecc78de0894a6fb4fa3876
Himanshu <75906597+himanshu-1808@users.noreply.github.com>
2021-05-22 21:39:06 +0530

    Added webpages

---
+++ src/components/Myjumbo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Myjumbo.css'
+function Myjumbo() {
+    return (
+        <div>
+            <div class="jumbotron ">
+                <div class="jumbotron  jumbo-picture">
+                    <div className="left col-lg-4"><h1>Prestwick Codes</h1>
+                        <p>And yes, you can change the background, colors, etc...</p>
+                        <p>Look at the code for this Jumbotron.</p>
+                        <p><a class="btn btn-primary btn-lg" href="http://prestwickcodes.org/" role="button">Learn more</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Myjumbo
--- src/components/cards.js
+++ src/components/cards.js
@@ -17,9 +17,6 @@ function cards() {
               <div class="card-info">
                <h2 class="card-title">Exploring around</h2>
                <p class="card-intro">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                <Link to='/yoga'>
-                 Click here
-                </Link>
              </div>
            </div>
        </div>
