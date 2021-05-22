f22b631440fab63337d157da9736db6c50684c6e
Yashita <yashita1109.cse19@chitkara.edu.in>
2021-05-22 21:40:56 +0530

    Yogapage added

--- src/components/Yoga.js
+++ src/components/Yoga.js
@@ -2,11 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Jumbo from './JumbotronYoga';
+import Crds from './YogaCards';
 function Yoga() {
   return (
     <div>
        <Navbar/>
        <Jumbo/>
+       <Crds/>
        <Footer/>
     </div>
   );
---
+++ src/components/YogaCards.js
@@ -0,0 +1,49 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './YogaCards.css'
+function YogaCards() {
+  return (
+    <div>
+        <div>
+            <h1 className="heading">F.A.C.T.S</h1>
+            </div>
+        <main>
+            
+        <section>
+
+    <figure>
+      <img src="https://images.pexels.com/photos/944743/pexels-photo-944743.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="Free Stock Photo from pexels.com"/>
+    </figure>
+    <article>
+      <span>Social Media</span>
+      <h1>Keep cool when you’re under attack</h1>
+      <p>Learn how to maintain your emotional balance when strangers start attacking you online.</p>
+    </article>
+  </section>
+  
+  <section>
+    <figure>
+      <img src="https://images.pexels.com/photos/821653/pexels-photo-821653.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="Free Stock Photo from pexels.com"/>
+    </figure>
+    <article>
+      <span>Photography</span>
+      <h1>Shooting pictures like professionals</h1>
+      <p>Exposure, rule of thirds, ISO: what do you really need to become a professional photographer?</p>
+    </article>
+  </section>
+  
+  <section>
+    <figure>
+      <img src="https://images.pexels.com/photos/1391421/pexels-photo-1391421.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="Free Stock Photo from pexels.com"/>
+    </figure>
+    <article>
+      <span>Travel</span>
+      <h1>Top 5 beaches to visit next holiday</h1>
+      <p>Time to change your destination and find out paradises to spend your next holiday.</p>
+    </article>
+  </section>
+  </main>
+    </div>
+  );
+}
+
+export default YogaCards;
